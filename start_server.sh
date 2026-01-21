b1e677d4877e141e1f430ca666ae11879149e9ba
gxy01953892 <gxy01953892@alibaba-inc.com>
2026-01-21 16:42:55 +0800

    fix

--- start_server.sh
+++ start_server.sh
@@ -4,4 +4,4 @@ export POOL_MAX_SIZE=100
 export POOL_MIN_SIZE=10
 export PREWARM_LANGUAGES=python
 
-sudo -E python3 -m llm_sandbox.mcp_server.mcp_server
+nohup sudo -E python3 -m llm_sandbox.mcp_server.mcp_server &
